e19b0c9ffa3db04ed756bb20c0e26da2d7be3788
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2024-07-04 11:55:56 +0530

    practice scanner class

--- java_practice/Practice_02.java
+++ java_practice/Practice_02.java
@@ -1,19 +1,31 @@
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Practice_02 {
     public static void main(String[] args) {
-        Random random = new Random();
+        /*
+         * Random random = new Random();
+         * Scanner sc = new Scanner(System.in);
+         * System.out.println("Enter your lucky number between 0 to 10: ");
+         * int yourNumber = sc.nextInt();
+         * int luckyNumber = random.nextInt(10);
+         * if (yourNumber == luckyNumber) {
+         * System.out.println("Congratulations! You won the lottery.");
+         * } else {
+         * System.out.println("Better luck next time. Lucky number was: " +
+         * luckyNumber);
+         * }
+         * sc.close();
+         * 
+         * // Shift OperaTor
+         * 
+         * System.out.println(10 << 2);
+         */
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your lucky number between 0 to 10: ");
-        int yourNumber = sc.nextInt();
-        int luckyNumber = random.nextInt(10);
-        if (yourNumber == luckyNumber) {
-            System.out.println("Congratulations! You won the lottery.");
-        } else {
-            System.out.println("Better luck next time. Lucky number was: " + luckyNumber);
-        }
-        sc.close();
+        int n = sc.nextInt();
+        boolean hasValue = sc.hasNextInt();
+        System.out.println(n);
+        System.out.println(hasValue);
+        System.out.println(sc.nextLine());
     }
 }
